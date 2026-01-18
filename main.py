b725aa59d96811efe7c3bb5f2f605f70112af34f
Arisnaldo <103151770+Arisnaldo@users.noreply.github.com>
2026-01-18 13:57:07 -0300

    Buscando preços

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+def buscar_preco_coinbase(moeda):
+    try:
+        #Api COINBASE
+        url = f"https://api.coinbase.com/v2/prices/BTC-{moeda}/spot"
+        
+        resposta = requests.get(url)
+        dados = resposta.json()
+        
+        preco = float(dados["data"]["amount"])
+        
+        print(f"Bitcoin em {moeda}: {preco:,.2f}")
+        return preco
+    except Exception as e:
+        print(f"Erro ao buscar dados: {e}")
+        return None
+
+# Testando com duas moedas
+buscar_preco_coinbase("USD")
+buscar_preco_coinbase("BRL")
